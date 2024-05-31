080c857533dd57004ff08ab101471c3aad9c2e7d
nilton <ngcquimica@gmail.com>
2024-05-31 17:30:01 -0300

    sqlc update

--- db/sqlc/models.go
+++ db/sqlc/models.go
@@ -17,16 +17,18 @@ type Account struct {
 }
 
 type Entry struct {
-	ID        int64     `json:"id"`
-	AccountID int64     `json:"account_id"`
+	ID        int64 `json:"id"`
+	AccountID int64 `json:"account_id"`
+	// can be negative or positive
 	Amount    int64     `json:"amount"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
 type Transfer struct {
-	ID            int64     `json:"id"`
-	ToAccountID   int64     `json:"to_account_id"`
-	FromAccountID int64     `json:"from_account_id"`
-	Amount        int64     `json:"amount"`
-	CreatedAt     time.Time `json:"created_at"`
+	ID            int64 `json:"id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	FromAccountID int64 `json:"from_account_id"`
+	// must be positive
+	Amount    int64     `json:"amount"`
+	CreatedAt time.Time `json:"created_at"`
 }
